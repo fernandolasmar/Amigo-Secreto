9c14793de9fe3cd4d88b2d6eef921522874b4778
Fernando ALP <f3rnandolasmar@gmail.com>
2025-02-12 07:12:01 +0800

    Adiciona uma função para adicionar amigos e função para limpar o input

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+
+// função para adicionar amigos
+function adicionarAmigo() {
+    let novoNome = document.getElementById("amigo").value;
+
+    if (novoNome == '') return alert('Por favor, insira um nome!');
+    
+    amigos.push(novoNome);
+    limparInput();
+    console.log(amigos)
+    
+}
+// Limpar campo do input
+function limparInput() {
+    let input = document.getElementById("amigo");
+    input.value = '';    
+}
